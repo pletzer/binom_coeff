8f218a8b5f5a81a4826edf3d7060acbd11fe4a5d
ALEXANDER PLETZER <axp5530@psu.edu>
2016-04-28 06:52:15 +1200

    minor improvements

--- test_binom_coeff.py
+++ test_binom_coeff.py
@@ -2,17 +2,18 @@ import sys
 from binom_coeff import binom_coeff
 
 if len(sys.argv) < 3:
-    print('Need at least 2 arguments')
+    print('Choose "k" out of "k"')
+    print('ERROR: need at least 2 arguments')
     print('Usage: {} n k'.format(sys.argv[0]))
-    print('choose "k" out of "k"')
     sys.exit(1)
 
-n = sys.argv[1]
-k = sys.argv[2]
+n = int(sys.argv[1])
+k = int(sys.argv[2])
 
 coeff = binom_coeff(n, k)
 
 print("""
-There are {0} possibilities to choose {1} elements out of a collection of {2}
+{0:10.0f} possibilities to choose {2} elements out of a collection of {1}
 """.format(coeff, n, k)
+)
 
